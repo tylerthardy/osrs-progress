329c6a242e0252cef60ac0a2aa042b01de008bbb
Tyler Hardy <tylerthardy@gmail.com>
2019-07-28 18:54:02 -0500

    Implement main page with progress bars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HiscoresService } from './hiscores/hiscores.service';
+import { HiscoreSkill } from './hiscores/hiscore-skill';
+import { Skill } from './hiscores/skill.enum';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,21 @@ import { HiscoresService } from './hiscores/hiscores.service';
 })
 export class AppComponent {
   title = 'osrs-progress';
+
+  public theSkill: HiscoreSkill = {
+    Skill: Skill.Woodcutting,
+    Rank: 1,
+    Level: 99
+  };
+
+  public skills: HiscoreSkill[];
+
+  constructor(private hiscoresService: HiscoresService) { }
+
+  ngOnInit(): void {
+    this.hiscoresService.GetSkills("perterter").subscribe((response: string) => {
+      this.skills = this.hiscoresService.GetHiscoreSkills(response);
+      this.theSkill.Xp = this.skills.filter(skill=>skill.Skill.Name == "Woodcutting")[0].Xp;
+    });
+  }
 }
