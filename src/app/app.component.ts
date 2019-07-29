480bfb6479b144f89e98ffe19d53f4a1fc222b9f
Tyler Hardy <tylerthardy@gmail.com>
2019-07-28 23:05:43 -0500

    Add username input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { HiscoresService } from './hiscores/hiscores.service';
 import { HiscoreSkill } from './hiscores/hiscore-skill';
-import { Skill } from './hiscores/skill.enum';
+import { FormControl } from '@angular/forms';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +12,31 @@ import { Skill } from './hiscores/skill.enum';
 export class AppComponent {
   title = 'osrs-progress';
 
-  public theSkill: HiscoreSkill = {
-    Skill: Skill.Woodcutting,
-    Rank: 1,
-    Level: 99
-  };
-
   public skills: HiscoreSkill[];
+  public username: FormControl;
+  public blurred: boolean;
 
   constructor(private hiscoresService: HiscoresService) { }
 
   ngOnInit(): void {
-    this.hiscoresService.GetSkills("perterter").subscribe((response: string) => {
+    this.username = new FormControl();
+  }
+
+  private populateSkills(username: string): void {
+    this.hiscoresService.GetSkills(username).subscribe((response: string) => {
       this.skills = this.hiscoresService.GetHiscoreSkills(response);
-      this.theSkill.Xp = this.skills.filter(skill=>skill.Skill.Name == "Woodcutting")[0].Xp;
+      this.blurred = false;
     });
+  }
+
+  public onUsernameChange(): void {
+    this.blurred = true;
+    if (!this.username.value) {
+      this.skills = null;
+      this.blurred = false;
+      return;
+    }
+
+    this.populateSkills(this.username.value);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
@@ -12,7 +17,12 @@ import { ProgressBarComponent } from './progress-bar/progress-bar.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+
+    BrowserAnimationsModule,
+
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
