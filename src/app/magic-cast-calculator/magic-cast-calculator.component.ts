9b068a11d6b0d715bc615250711b0e0de5d51200
Tyler Hardy <tylerthardy@gmail.com>
2022-06-22 02:36:37 -0400

    Add settings for magic calc

--- src/app/magic-cast-calculator/magic-cast-calculator.component.ts
+++ src/app/magic-cast-calculator/magic-cast-calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { environment } from '../../environments/environment';
 import { HiscoreSkill } from '../hiscores/hiscore-skill';
 import { Skill } from '../hiscores/skill.enum';
@@ -14,6 +15,8 @@ export interface IMagicCastRow {
   casts?: number;
 }
 
+const CASTS_PER_HOUR = 1200;
+
 @Component({
   selector: 'app-magic-cast-calculator',
   templateUrl: './magic-cast-calculator.component.html',
@@ -24,15 +27,15 @@ export class MagicCastCalculatorComponent implements OnInit {
   private isProduction = environment.production;
 
   public dataSource: IMagicCastRow[] = [
-    { spell: 'Fire Strike', cost: 305, xpPerThousandCasts: 19416, xpPerHour: 23300 },
-    { spell: 'Fire Bolt', cost: 400, xpPerThousandCasts: 45200, xpPerHour: 50200 },
-    { spell: 'Fire Blast', cost: 525, xpPerThousandCasts: 56200, xpPerHour: 67300 },
-    { spell: 'Fire Wave', cost: 655, xpPerThousandCasts: 61333, xpPerHour: 69500 },
+    { spell: 'Fire Strike', cost: 305, xpPerThousandCasts: -1, xpPerHour: 23300 },
+    { spell: 'Fire Bolt', cost: 400, xpPerThousandCasts: -1, xpPerHour: 50200 },
+    { spell: 'Fire Blast', cost: 525, xpPerThousandCasts: -1, xpPerHour: 67300 },
+    { spell: 'Fire Wave', cost: 655, xpPerThousandCasts: -1, xpPerHour: 69500 },
   ];
 
   public form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
@@ -42,9 +45,9 @@ export class MagicCastCalculatorComponent implements OnInit {
       targetXp: Skill.MAX_XP,
       xpDifference: { value: 0, disabled: true },
     });
-    this.form.valueChanges.subscribe(() => {
-      this.calculateRates();
-    });
+    this.form.get('targetLevel').valueChanges.subscribe(() => this.calculateRates());
+    this.form.get('currentXp').valueChanges.subscribe(() => this.calculateRates());
+    this.calculateRates();
   }
 
   public calculateRates(): void {
@@ -56,6 +59,7 @@ export class MagicCastCalculatorComponent implements OnInit {
       currentLevel: LevelsService.getLevelFromXp(this.form.get('currentXp').value),
     });
     this.dataSource.forEach((row: IMagicCastRow) => {
+      row.xpPerThousandCasts = (row.xpPerHour / CASTS_PER_HOUR) * 1000;
       row.hours = xpDifference / row.xpPerHour;
       row.casts = (xpDifference / row.xpPerThousandCasts) * 1000;
     });
@@ -66,11 +70,28 @@ export class MagicCastCalculatorComponent implements OnInit {
     const level: number = this.form.get('currentLevel').value;
     const xp: number = this.form.get('currentXp').value;
     const csv = `${rank},${level},${xp}`;
-    console.log(csv);
     return new HiscoreSkill(Skill.Magic, csv);
   }
 
   public getTargetXp(): number {
     return this.form.get('targetXp').value;
+  }
+
+  public toggleSettings(): void {
+    if (this.displayedColumns[0] === 'adjust') {
+      this.displayedColumns.shift();
+    } else {
+      this.displayedColumns.unshift('adjust');
+    }
+  }
+
+  public adjust(row: IMagicCastRow): void {
+    let input: string = prompt('XP/hr', row.xpPerHour.toString());
+    let result: number = Number.parseInt(input);
+    if (isNaN(result)) {
+      return;
+    }
+    row.xpPerHour = result;
+    this.calculateRates();
   }
 }
--- src/app/magic-cast-calculator/magic-cast-calculator.module.ts
+++ src/app/magic-cast-calculator/magic-cast-calculator.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { ProgressBarModule } from '../progress-bar/progress-bar.module';
@@ -11,7 +12,15 @@ import { MagicCastCalculatorService } from './magic-cast-calculator.service';
 @NgModule({
   exports: [MagicCastCalculatorComponent],
   declarations: [MagicCastCalculatorComponent],
-  imports: [CommonModule, ReactiveFormsModule, MatTableModule, MatInputModule, MatButtonModule, ProgressBarModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    ProgressBarModule,
+  ],
   providers: [MagicCastCalculatorService],
 })
 export class MagicCastCalculatorModule {}
